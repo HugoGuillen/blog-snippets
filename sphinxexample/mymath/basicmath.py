d13678a2152ec8fd929818ea8e355021cc389d1f
Hugo Guillen <hugoagr@gmail.com>
2016-04-07 12:16:52 -0700

    sphinx example

---
+++ sphinxexample/mymath/basicmath.py
@@ -0,0 +1,21 @@
+"""
+basicmath.py:
+    Reimplementing basic operations for no purpose at all.
+"""
+__author__ = "Hugo Guillen"
+__maintainer__ = "Hugo Guillen"
+__email__ = "hugoagr@gmail.com"
+__copyright__ = "Copyright 2016"
+__version__ = "0.1"
+
+def sum_two_numbers(x,y):
+	"""
+	Sums two numbers.
+	
+	:param x: first number.
+	
+	:param y: second number.
+	
+	:returns: the result of x+y.
+	"""
+	return x+y
